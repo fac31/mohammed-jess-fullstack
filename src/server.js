ba6f314e79784ccc785fa0b64ccc3a81938ef018
ShMuhammad <14.chaouni@gmail.com>
2024-05-29 18:04:04 +0100

    Login form updated

--- src/server.js
+++ src/server.js
@@ -16,23 +16,49 @@ app.get('/about', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/html/about.html'))
 })
 
-//----------Register a user----------------------------------
+//------------------------Register a user----------------------------------
 app.post('/register', async (req, res) => {
     const data = {
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
     }
-    try {
-        const userData = await collection.insertMany(data)
-        console.log(userData)
-        res.status(201).send('User registered successfully!')
-    } catch (error) {
-        console.error(error)
-        res.status(500).send('Error inserting data')
+
+    // Check if the username exists in the database
+    const existingUser = await collection.findOne({ name: data.name })
+    if (existingUser) {
+        res.send('User name already exists. Please choose a different name.')
+    } else {
+        try {
+            const userData = await collection.insertMany(data)
+            console.log(userData)
+            res.send('User registered successfully!')
+        } catch (error) {
+            console.error(error)
+            res.send('Error inserting data')
+        }
     }
 })
-//----------------------------------------------------------
+//---------------------------------------------------------------------------
+//-----------------------------Login user------------------------------------
+app.post('/login', async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.name })
+
+        if (!check) {
+            return res.status(401).send('User not found')
+        }
+
+        if (check.password === req.body.password) {
+            res.send('Login successfully')
+        } else {
+            res.send('Wrong password')
+        }
+    } catch (error) {
+        res.send('Wrong details!')
+    }
+})
+//---------------------------------------------------------------------------
 
 app.listen(4000, () => {
     console.log('server is running')
